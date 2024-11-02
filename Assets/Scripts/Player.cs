9709311627e20b2e44d6f9aa169808f640555d24
Hannah Fisher <hannahtfisher@gmail.com>
2024-11-02 22:26:23 +0000

    Added basic left and right player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float movementSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,13 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * movementSpeed);
+        }
     }
 }
