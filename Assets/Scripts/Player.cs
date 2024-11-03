bd11acf926e69200c63a7fde138172b4fcd02340
Hannah Fisher <hannahtfisher@gmail.com>
2024-11-03 19:17:53 +0000

    Added default physic materials and updated player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,20 +6,21 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private GameplayConfig gameplayConfig;
 
+    [SerializeField] private Rigidbody rb;
+
     private float movementSpeed;
     private float jump;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        if (gameplayConfig != null)
-        {
-            InitialiseGameplayConfig();
-        }
-        else
+        if (gameplayConfig == null)
         {
             Debug.LogError("Error: GameplayConfig is missing a reference for the Player script!");
+            return;
         }
+
+        InitialiseGameplayConfig();
     }
 
     private void InitialiseGameplayConfig()
@@ -28,20 +29,31 @@ public class Player : MonoBehaviour
         jump = gameplayConfig.PlayerJumpHeight;
     }
 
-        // Update is called once per frame
-        void Update()
+    // Update is called once per frame
+    private void Update()
     {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        Vector3 playerMovement = default; // start at default 0 value
+
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(Vector3.left * Time.deltaTime * movementSpeed);
+            playerMovement += Vector3.left * movementSpeed;
         }
+
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(Vector3.right * Time.deltaTime * movementSpeed);
+            playerMovement += Vector3.right * movementSpeed;
         }
+
         if (Input.GetKey(KeyCode.Space))
         {
-            transform.Translate(Vector3.up * Time.deltaTime * jump);
+            playerMovement += Vector3.up * jump;
         }
+
+        rb.MovePosition(rb.position + playerMovement * Time.deltaTime);
     }
 }
